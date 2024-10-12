a03351d0cab5814f6621c2c52a1729ffddb7b77e
Abu Jehoiada <abujehoiada@gmail.com>
2024-10-12 01:18:53 +0100

    Fixed code

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Module for the add function'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Add function for just two arguments
+    Arguments:
+    str1: First string
+    str2: Second string
+    '''
+    return (str1 + str2)
