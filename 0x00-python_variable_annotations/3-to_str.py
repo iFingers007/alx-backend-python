c789893d19b5f2cdbef8d42696a71bea0c6a7a96
Abu Jehoiada <abujehoiada@gmail.com>
2024-10-12 01:26:22 +0100

    Fixed code

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Module for the to string function'''
+
+
+def to_str(n: float) -> str:
+    '''Add function for just two arguments
+    Arguments:
+    n: number
+    '''
+    return str(n)
