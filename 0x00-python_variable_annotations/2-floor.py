6f0f943114eea73bdfbddfe0dd9a10000384206a
Abu Jehoiada <abujehoiada@gmail.com>
2024-10-12 01:24:06 +0100

    Fixed code

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Module for the floor division function'''
+
+
+def floor(n: float) -> int:
+    '''Add function for just two arguments
+    Arguments:
+    n: number
+    '''
+    return int(n)
