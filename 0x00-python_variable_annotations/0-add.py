a2c33ad7404d9108e3bbabec5cfcf41b3c155d5f
Abu Jehoiada <abujehoiada@gmail.com>
2024-10-12 01:15:37 +0100

    Fixed code

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Module for the add function'''
+
+
+def add(a: float, b: float) -> float:
+    '''Add function for just two arguments
+    Arguments:
+    a: First number
+    b: Second Number
+    '''
+    return (a + b)
