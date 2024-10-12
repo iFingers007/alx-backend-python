29789085c2cfe012667f861b5efeae946234b148
Abu Jehoiada <abujehoiada@gmail.com>
2024-10-12 01:40:37 +0100

    Fixed code

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Module for Complex types - list of floats'''
+
+
+def sum_list(input_list: list[float]) -> float:
+    '''sums a list and gives the sum as return
+    Arguments:
+    input_list: list inputed
+    '''
+    return sum(input_list)
