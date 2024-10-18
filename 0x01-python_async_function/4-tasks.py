60a0bae92a03a6a3e84c146755f90d4075c8fd45
Abu Jehoiada <abujehoiada@gmail.com>
2024-10-18 21:18:57 +0100

    Fixed code

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+''' Module for 4-tasks.py'''
+
+import asyncio
+from typing import List
+from 3_tasks import task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    '''Take the code from wait_n and alter it into a new function task_wait_n
+    Args:
+    n: Number of times
+    max_delay: Maximum delay
+    '''
+    # Create list of asyncio.Tasks
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    # Gather all tasks and wait for them to complete
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)  # Return delays sorted in ascending order
